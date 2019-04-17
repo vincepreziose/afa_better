aa92aef8b49e58be03d0d6100d5af414e4a8f529
vince preziose <vince@softphusion.com>
2019-04-16 20:48:22 -0700

    fix redirect to map after logout

---
+++ src/components/Auth/Signin/index.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import { reduxForm, Field } from 'redux-form';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
+import * as actions from '../../../actions';
+
+class Signin extends Component {
+  onSubmit = (formProps) => {
+    this.props.signin(formProps, () => {
+      this.props.history.push('/admin');
+    });
+  };
+
+  render() {
+    const { handleSubmit } = this.props;
+
+    return (
+      <form onSubmit={handleSubmit(this.onSubmit)}>
+        <fieldset>
+          <label htmlFor="email">Email</label>
+          <Field
+            name="email"
+            type="text"
+            component="input"
+            autoComplete="none"
+          />
+        </fieldset>
+        <fieldset>
+          <label htmlFor="password">Password</label>
+          <Field
+            name="password"
+            type="password"
+            component="input"
+            autoComplete="none"
+          />
+        </fieldset>
+        <div>{this.props.errorMessage}</div>
+        <button>Sign In!</button>
+      </form>
+    )
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    errorMessage: state.auth.errorMessage
+  }
+}
+
+export default compose(connect(mapStateToProps, actions), reduxForm({ form: 'signin' }))(
+  Signin
+);
