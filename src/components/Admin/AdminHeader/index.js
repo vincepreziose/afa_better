088b7e281f0ef4ccb45eabc13c6ca46273dacaec
vince preziose <vince@softphusion.com>
2019-04-23 17:55:32 -0700

    feature/admin-panel + add lab profile view.

--- src/components/Admin/AdminHeader/index.js
+++ src/components/Admin/AdminHeader/index.js
@@ -6,8 +6,7 @@ const AdminHeader = ({ headerText }) => {
       <h1 className="h2">{ headerText }</h1>
       <div className="btn-toolbar mb-2 mb-md-0">
         <div className="btn-group mr-2">
-          <button className="btn btn-sm btn-outline-secondary">Share</button>
-          <button className="btn btn-sm btn-outline-secondary">Export</button>
+          <button className="btn btn-sm btn-outline-secondary">Edit</button>
         </div>
       </div>
     </div>
--- src/components/Admin/LabView/index.js
+++ src/components/Admin/LabView/index.js
@@ -5,6 +5,7 @@ import _ from 'lodash';
 import AdminSidebar from '../AdminSidebar';
 import AdminHeader from '../AdminHeader';
 import {getLabs} from "../../../actions";
+import styles from './LabView.module.css';
 
 class LabView extends Component {
   state = {
@@ -23,13 +24,78 @@ class LabView extends Component {
     console.log('...Loading')
   }
 
+  renderReportData(data) {
+    let rows = [];
+    data.forEach((row, i) => {
+      rows.push(
+        <tr key={i}>
+          <td>{row.a}</td>
+          <td>{row.b}</td>
+          <td>{row.c}</td>
+          <td>{row.d}</td>
+          <td>{row.e}</td>
+          <td>{row.notes}</td>
+        </tr>
+      )
+    });
+
+    return (
+      rows
+    )
+  }
+
   renderLab() {
     const arg = this.state;
     let lab = this.props.labs.find((lab) => {
       return lab.id === arg.labId
     }, this.state);
 
-    console.log(lab)
+    // console.log(lab)
+
+    if (lab) {
+      return (
+        <div>
+          <hr/>
+          <div className={styles.LabInfoHeader}>
+            <div className={styles.Row}>
+              <div className={styles.Column}>
+                <div className={styles.ColumnOne}>
+                  <h5 className={styles.BottomPush}>Name: <br /><span className={styles.LabInfoText}>{lab.name}</span></h5>
+                  <h5 className={styles.BottomPush}>Address1: <br/> <span className={styles.LabInfoText}>{lab.address1}</span></h5>
+                  <h5>Address2: <br/><span className={styles.LabInfoText}>{lab.address2}</span></h5>
+                </div>
+              </div>
+              <div className={styles.Column}>
+                <div className={styles.ColumnTwo}>
+                  <h5 className={styles.BottomPush}>City: <br/><span className={styles.LabInfoText}>{lab.city}</span></h5>
+                  <h5>Certificate Number: <br/><span className={styles.LabInfoText}>{lab.certificateNum}</span></h5>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <hr/>
+          <h4 className={styles.ReportDataHeader}>Report Data</h4>
+          <div className="table-responsive">
+            <table className="table table-striped table-sm">
+              <thead>
+              <tr>
+                <th>A</th>
+                <th>B</th>
+                <th>C</th>
+                <th>D</th>
+                <th>E</th>
+                <th>Notes</th>
+              </tr>
+              </thead>
+              <tbody>
+              { this.renderReportData(lab.reportData) }
+              </tbody>
+            </table>
+          </div>
+        </div>
+      );
+    }
   }
 
   render() {
